c78b7093938db837327757112f7422e290898bab
Richard Gagg <r.gagg@outlook.com>
2025-01-09 13:05:54 +1100

    Completed Elvis challenge

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var message: String = ""
+  @State private var imageName: String = ""
+  
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          Text("What's So Funny 'Bout")
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundStyle(.purple)
+          
+          Spacer()
+          
+          Image(systemName: imageName)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 300, height: 300)
+          
+          Text(message)
+            .font(.largeTitle)
+            .fontWeight(.black)
+            .foregroundStyle(.purple)
+          
+          Spacer()
+          
+          HStack {
+            Button {
+              imageName = "peacesign"
+              message = "Peace"
+            } label: {
+              Text("Peace")
+            }
+            
+            Button {
+              imageName = "heart"
+              message = "Love"
+            } label: {
+              Text("Love")
+            }
+            
+            Button {
+              imageName = "lightbulb"
+              message = "Understanding"
+            } label: {
+              Text("Understanding")
+            }
+          }
+          .buttonStyle(.borderedProminent)
+          .font(.title2)
+          .tint(.purple)
+          
         }
         .padding()
     }
